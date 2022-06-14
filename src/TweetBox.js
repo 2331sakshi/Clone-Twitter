99fff44f4a2800986ac9c82df96b19ed537914df
2331sakshi <skshpanchal@gmail.com>
2022-06-14 19:01:37 +0530

    Updated App.css, TweetBox.js & Widgets.js

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -11,43 +11,26 @@ function TweetBox() {
         e.preventDefault(); //stops the refresh of page
 
         db.collection('posts').add({
-<<<<<<< HEAD
             displayName: 'Sakshi Panchal',
             username: "sakshi2331",
-=======
-            displayName: 'Sai Keshari1',
-            username: "saikeshari1",
->>>>>>> 847da22f91f53cc2fa83513abf4911118af87efb
             verified: true,
             text : tweetMessage,
             image: tweetImage,
             avatar: "https://images.app.goo.gl/Y7etdAXaWePfsebL8"
         });
         setTweetImage("");
-<<<<<<< HEAD
-        setTweetMessage(""); 
-=======
         setTweetMessage("");
->>>>>>> 847da22f91f53cc2fa83513abf4911118af87efb
     }
     return (
         <div className="tweetBox">
             <form>
                 <div className="tweetBox__input">
-<<<<<<< HEAD
-                    <Avatar>SP</Avatar>
-=======
                     <Avatar>SK</Avatar>
->>>>>>> 847da22f91f53cc2fa83513abf4911118af87efb
                     <input 
                         onChange={(e) => setTweetMessage(e.target.value)}
                         value={tweetMessage} 
                         placeholder="What's happening" 
-<<<<<<< HEAD
-                        type="text"
-=======
                         type="text" 
->>>>>>> 847da22f91f53cc2fa83513abf4911118af87efb
                     />
                 </div>
                 <input 
--- src/Widgets.js
+++ src/Widgets.js
@@ -12,17 +12,12 @@ function Widgets() {
         <div className="widgets">
             <div className="widgets__input">
                 <SearchIcon className="widgets__searchIcon" />
-<<<<<<< HEAD
                 <input placeholder="Search Twitter" type="text" />
-=======
-                <input placeholder="Search Twitter?" type="text" />
->>>>>>> 847da22f91f53cc2fa83513abf4911118af87efb
             </div>
             <div className="widgets__widgetContainer">
                 <h2>Whats Happening?</h2>
 
                 <TwitterTweetEmbed tweetId={"1332305023888363528"} />
-<<<<<<< HEAD
                 <TwitterTweetEmbed tweetId={"1333677773357944834"} />
                 <TwitterTweetEmbed tweetId={"1333674416882864129"} />
 
@@ -34,19 +29,6 @@ function Widgets() {
 
                 <TwitterShareButton url="https://github.com/2331sakshi"
                     options={{ text:"reactjs is awesome", via:"sakshipanchal"}}
-=======
-                {/* <TwitterTweetEmbed tweetId={"1333677773357944834"} /> */}
-                {/* <TwitterTweetEmbed tweetId={"1333674416882864129"} /> */}
-
-                <TwitterTimelineEmbed 
-                    soruceType="profile"
-                    screenName="saikeshari"
-                    options={{ height:400 }}
-                />
-
-                <TwitterShareButton url="https://github.com/saikeshari"
-                    options={{ text:"reactjs is awesome", via:"saikeshari"}}
->>>>>>> 847da22f91f53cc2fa83513abf4911118af87efb
                 />
             </div>
         </div>
