59336358bf425cf778b61a1541043427e5ca9bd2
2331sakshi <skshpanchal@gmail.com>
2022-06-14 19:18:55 +0530

    Create firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,4 @@
 import firebase from "firebase";
-<<<<<<< HEAD
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
@@ -14,25 +13,8 @@ const firebaseConfig = {
   measurementId: "G-6Q4E30QZ5B"
 
 };
-=======
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyDTemTbUsQy0v5z0C1PxiagTXdnfnK2NpY",
-    authDomain: "twitter-clone-23d8e.firebaseapp.com",
-    databaseURL: "https://twitter-clone-23d8e.firebaseio.com",
-    projectId: "twitter-clone-23d8e",
-    storageBucket: "twitter-clone-23d8e.appspot.com",
-    messagingSenderId: "965727023475",
-    appId: "1:965727023475:web:3244205051299fa102d506",
-    measurementId: "G-RFBSW2TEWX"
-  };
->>>>>>> 847da22f91f53cc2fa83513abf4911118af87efb
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
 
   const db=firebaseApp.firestore();
-<<<<<<< HEAD
-=======
-
->>>>>>> 847da22f91f53cc2fa83513abf4911118af87efb
   export default db;
